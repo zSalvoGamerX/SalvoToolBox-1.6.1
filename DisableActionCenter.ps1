bb744a59bcc8ce0217798e6ea2694ad78b5c59a7
zSalvoGamerX <85034298+zSalvoGamerX@users.noreply.github.com>
2021-06-02 12:59:14 +0200

    added needed files for toolbox to run normally

---
+++ DisableActionCenter.ps1
@@ -0,0 +1,7 @@
+# Disable Action Center
+ Write-Host "Disabling Action Center..."
+ If (!(Test-Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer")) {
+ New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" | Out-Null
+ }
+ Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
+ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Type DWord -Value 0
